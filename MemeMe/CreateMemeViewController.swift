f0ef8f0aaa1dc13d6327c35dc36cbcb739ca0143
JUNYEONG.YOO <woody.chiz@gmail.com>
2017-01-24 19:10:35 +0900

    Delete NavigationController from CreateMemeViewController
    - Alter the content view to scroll view

--- MemeMe/CreateMemeViewController.swift
+++ MemeMe/CreateMemeViewController.swift
@@ -23,12 +23,14 @@ class CreateMemeViewController: UIViewController, UIImagePickerControllerDelegat
 
 	// MARK: IBOutlets
 	
+	@IBOutlet weak var scrollView: UIScrollView!
 	@IBOutlet weak var cameraButton: UIBarButtonItem!
 	@IBOutlet weak var shareButton: UIBarButtonItem!
 	@IBOutlet weak var cancelButton: UIBarButtonItem!
 	@IBOutlet weak var topTextField: UITextField!
 	@IBOutlet weak var bottomTextField: UITextField!
 	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var initialLabel: UILabel!
 
 	// MARK: Life cycle of View Controller
 	
@@ -36,6 +38,9 @@ class CreateMemeViewController: UIViewController, UIImagePickerControllerDelegat
 		super.viewDidLoad()
 		self.topTextField.delegate = self
 		self.bottomTextField.delegate = self
+		
+		// Hide keyboard when a user touches screen
+		self.scrollView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
 	}
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -43,15 +48,33 @@ class CreateMemeViewController: UIViewController, UIImagePickerControllerDelegat
 		
 		// Check whether the application could use camera
 		self.cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+
+		// Disable share button initially
 		self.shareButton.isEnabled = false
 		
-		// TODO: delete this code when CreateMemeViewController is used at the main view controller
-		self.cancelButton.isEnabled = false
-		
+		// Set attributes of the text field
 		self.topTextField.defaultTextAttributes = memeTextAttributes
 		self.topTextField.textAlignment = .center
 		self.bottomTextField.defaultTextAttributes = memeTextAttributes
 		self.bottomTextField.textAlignment = .center
+		
+		// Hide text field initially
+		self.topTextField.isHidden = true
+		self.bottomTextField.isHidden = true
+		
+		// Subscribe keyboard notification
+		self.subscribeToKeyboardNotification()
+		
+		// TODO: delete this code when CreateMemeViewController is used at the main view controller
+		self.cancelButton.isEnabled = false
+		
+			}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		
+		// Unsubscribe keyboard notification
+		self.unsubscribeToKeyboardNotification()
 	}
 	
 	// MARK: IBActions
@@ -88,6 +111,9 @@ class CreateMemeViewController: UIViewController, UIImagePickerControllerDelegat
 		
 		self.dismiss(animated: true) {
 			self.shareButton.isEnabled = true
+			self.initialLabel.isHidden = true
+			self.topTextField.isHidden = false
+			self.bottomTextField.isHidden = false
 		}
 	}
 	
@@ -116,9 +142,47 @@ class CreateMemeViewController: UIViewController, UIImagePickerControllerDelegat
 		return true
 	}
 	
-	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+	// MARK: Subscription to Keyboard showing up or hiding
+	
+	func subscribeToKeyboardNotification() {
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+	}
+	
+	func unsubscribeToKeyboardNotification() {
+		NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+		NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+	}
+	
+	func keyboardWillShow(_ notification: Notification) {
+		let userInfo = notification.userInfo!
+		if let keyboardSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size {
+			let contentInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize.height, 0.0)
+			self.scrollView.contentInset = contentInsets
+			self.scrollView.scrollIndicatorInsets = contentInsets
+			
+			var aRect = self.view.frame
+			aRect.size.height -= keyboardSize.height
+			
+			if let activeTextField = self.activeTextField {
+				if !(aRect.contains(activeTextField.frame.origin)) {
+					self.scrollView.scrollRectToVisible(activeTextField.frame, animated: true)
+				}
+			}
+		}
+	}
+	
+	func keyboardWillHide(_ notification: Notification) {
+		let contentInsets = UIEdgeInsets.zero
+		self.scrollView.contentInset = contentInsets
+		self.scrollView.scrollIndicatorInsets = contentInsets
+	}
+	
+	func hideKeyboard() {
 		self.view.endEditing(true)
-		self.activeTextField = nil
+		if self.activeTextField != nil {
+			self.activeTextField = nil
+		}
 	}
 }
 
